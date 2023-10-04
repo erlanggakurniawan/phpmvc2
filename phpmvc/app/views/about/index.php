1f21ebfe7450b7c72886669c35115888869607ed
erlanggakurniawan <142557097+erlanggakurniawan@users.noreply.github.com>
2023-10-04 13:09:35 +0700

    phpmvc ke 2

---
+++ phpmvc/app/views/about/index.php
@@ -0,0 +1,8 @@
+<div class="container mt-4">
+    <div class="d-flex flex-column align-items-center gap-4">
+        <h1>About Me</h1>
+        <img src="<?= BASEURL; ?>/img/Erlangga.jpg" width="200" class="rounded-circle shadow">
+        <p>Hallo nama saya <?= $data['nama']; ?>, saya adalah seorang <?= $data['status']; ?>, umur saya adalah
+            <?= $data['umur']; ?> tahun</p>
+    </div>
+</div>
